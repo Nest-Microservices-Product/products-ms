173f7aaf7f8bfdc9071c19aae3b78775ba5f0d5e
FernandoEmilianoFloresDeLaRiva <emilianoflores07081@gmail.com>
2024-04-24 09:26:20 -0600

    fix errors for transport nats

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     {
       transport: Transport.NATS,
       options: {
-        ervers: envs.natsServers,
+        servers: envs.natsServers,
       },
     },
   );
