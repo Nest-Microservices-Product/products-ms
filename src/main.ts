7b07c94beb294d33c29cf2c91cd474c8fb6ac248
FernandoEmilianoFloresDeLaRiva <emilianoflores07081@gmail.com>
2025-05-26 21:42:25 -0600

    Update Jenkinsfile for improved deployment configuration

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
     }),
   );
   await app.listen();
-  logger.log(`Products microservice running`);
+  logger.log(`Products microservice running on prod`);
 }
 
 bootstrap();
